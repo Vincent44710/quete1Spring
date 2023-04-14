d58f362a9494cc4c85a5fff521ac47419a7bd78b
Vincent <vinto0410@gmail.com>
2023-04-14 16:54:44 +0200

    quete1

---
+++ springinitializr/src/test/java/com/quete1/springinitializr/SpringinitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quete1.springinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
